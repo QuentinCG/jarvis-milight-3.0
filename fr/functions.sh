18793466bcf7db8eb5e3f33691036bc33fc812a9
Quentin Comte-Gaz <quentin@comte-gaz.com>
2017-04-15 18:02:24 +0200

    plugin: Improve message to user

--- fr/functions.sh
+++ fr/functions.sh
@@ -6,7 +6,7 @@ jv_pg_m3_lang()
     mac_address_is) echo "L'adresse mac du module milight est $2";;
     invalid_mac_address) echo "Impossible de récupérer l'adresse mac du module milight...";;
     light_on_success) echo "Lumières $2 allumées";;
-    light_on_failed) echo "Echec de l'allumage des lumières $2: $3";;
+    light_on_failed) echo "Echec de l'allumage des lumières $2";;
     *) jv_error "Erreur: Clef de traduction '$1' non reconnue. Merci de contacter le concepteur de ce plugin.";;
   esac
 }
--- functions.sh
+++ functions.sh
@@ -1,23 +1,25 @@
 #!/usr/bin/env bash
 
 # $1: zone id
-# return: zone name
+# $2: zone name (return value)
 jv_pg_m3_zone_id_to_zone_name()
 {
   case "$1" in
-    0) return $var_jv_pg_m3_all_zone_name;;
-    1) return $var_jv_pg_m3_zone_1_name;;
-    2) return $var_jv_pg_m3_zone_2_name;;
-    3) return $var_jv_pg_m3_zone_3_name;;
-    4) return $var_jv_pg_m3_zone_4_name;;
+    0) eval "$2='$var_jv_pg_m3_all_zone_name'";;
+    1) eval "$2='$var_jv_pg_m3_zone_1_name'";;
+    2) eval "$2='$var_jv_pg_m3_zone_2_name'";;
+    3) eval "$2='$var_jv_pg_m3_zone_3_name'";;
+    4) eval "$2='$var_jv_pg_m3_zone_4_name'";;
     *) jv_error "Error: Zone ID '$1' does not exist, don't use it in command file!";;
   esac
 }
 
 jv_pg_m3_getMacAddress()
 {
+  # Send request to milight module
   result="$(python3 plugins/jarvis-milight-3.0/MilightWifiBridge/MilightWifiBridge.py --ip $var_jv_pg_m3_ip --port $var_jv_pg_m3_port --timeout $var_jv_pg_m3_request_timeout_in_sec --getMacAddress)"
 
+  # Show the result to user
   if [[ $result =~ ^[0-9a-fA-F]{2}:[0-9a-fA-F]{2}:[0-9a-fA-F]{2}:[0-9a-fA-F]{2}:[0-9a-fA-F]{2}:[0-9a-fA-F]{2}$ ]]; then
     say "$(jv_pg_m3_lang mac_address_is $result)"
   else
@@ -28,11 +30,17 @@ jv_pg_m3_getMacAddress()
 # $1: zone id
 jv_pg_m3_lightOnWhite()
 {
+  # Send request to milight module
   result="$(python3 plugins/jarvis-milight-3.0/MilightWifiBridge/MilightWifiBridge.py --ip $var_jv_pg_m3_ip --port $var_jv_pg_m3_port --timeout $var_jv_pg_m3_request_timeout_in_sec --zone $1 --turnOn --setWhiteMode)"
 
+  # Get the zone name
+  zone_name=""
+  jv_pg_m3_zone_id_to_zone_name $1 $zone_name
+
+  # Show the result to user
   if [[ $result == "" ]]; then
-    say "$(jv_pg_m3_lang light_on_success $(jv_pg_m3_zone_id_to_zone_name $1))"
+    say "$(jv_pg_m3_lang light_on_success $zone_name)"
   else
-    say "$(jv_pg_m3_lang light_on_failed $(jv_pg_m3_zone_id_to_zone_name $1) $result)"
+    say "$(jv_pg_m3_lang light_on_failed $zone_name)"
   fi
 }
