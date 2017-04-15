e1efc6009fdf283c07fc88342d57f9a1ea3c5449
Quentin Comte-Gaz <quentin@comte-gaz.com>
2017-04-15 17:32:28 +0200

    plugin: Add light on function

--- fr/functions.sh
+++ fr/functions.sh
@@ -5,6 +5,8 @@ jv_pg_m3_lang()
   case "$1" in
     mac_address_is) echo "L'adresse mac du module milight est $2";;
     invalid_mac_address) echo "Impossible de récupérer l'adresse mac du module milight...";;
+    light_on_success) echo "Lumières $2 allumées";;
+    light_on_failed) echo "Echec de l'allumage des lumières $2: $3";;
     *) jv_error "Erreur: Clef de traduction '$1' non reconnue. Merci de contacter le concepteur de ce plugin.";;
   esac
 }
--- functions.sh
+++ functions.sh
@@ -10,3 +10,15 @@ jv_pg_m3_getMacAddress()
     say "$(jv_pg_m3_lang invalid_mac_address)"
   fi
 }
+
+# $1: zone
+jv_pg_m3_lightOnWhite()
+{
+  result="$(python3 plugins/jarvis-milight-3.0/MilightWifiBridge/MilightWifiBridge.py --ip $var_jv_pg_m3_ip --port $var_jv_pg_m3_port --timeout $var_jv_pg_m3_request_timeout_in_sec --zone $1 --turnOn --setWhiteMode)"
+
+  if [[ $result == "" ]]; then
+    say "$(jv_pg_m3_lang light_on_success $1)"
+  else
+    say "$(jv_pg_m3_lang light_on_failed $1 $result)"
+  fi
+}
