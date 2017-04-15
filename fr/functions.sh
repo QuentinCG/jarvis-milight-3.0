ab3f02bc6737384b66ec1cd93ed4c47b06d6c420
Quentin Comte-Gaz <quentin@comte-gaz.com>
2017-04-15 18:46:29 +0200

    plugin: Fix minor issue in french translation

--- fr/functions.sh
+++ fr/functions.sh
@@ -5,8 +5,8 @@ jv_pg_m3_lang()
   case "$1" in
     mac_address_is) echo "L'adresse mac du module milight est $2";;
     invalid_mac_address) echo "Impossible de récupérer l'adresse mac du module milight...";;
-    light_on_success) zone_name=""; jv_pg_m3_zone_id_to_zone_name $2 $zone_name; echo "Lumières $zone_name allumées";;
-    light_on_failed) zone_name=""; jv_pg_m3_zone_id_to_zone_name $2 $zone_name; echo "Echec de l'allumage des lumières $zone_name";;
+    light_on_success) zone_name=""; jv_pg_m3_zone_id_to_zone_name $2 zone_name; echo "$zone_name allumée";;
+    light_on_failed) zone_name=""; jv_pg_m3_zone_id_to_zone_name $2 zone_name; echo "Echec de l'allumage des lumières de la zone $zone_name";;
     *) jv_error "Erreur: Clef de traduction '$1' non reconnue. Merci de contacter le concepteur de ce plugin.";;
   esac
 }
