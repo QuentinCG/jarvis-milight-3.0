77ca5782249c21fe1ff67ca5f2d48e67ee0dbf7f
Quentin Comte-Gaz <quentin@comte-gaz.com>
2017-04-15 17:44:02 +0200

    plugin: Improve light on implementation

--- config.sh
+++ config.sh
@@ -2,6 +2,7 @@ var_jv_pg_m3_ip="192.168.1.23" # Milight wifi bridge IP
 var_jv_pg_m3_port=5987 # Milight wifi bridge port
 var_jv_pg_m3_request_timeout_in_sec=5 # Milight wifi bridge communication timeout
 
+var_jv_pg_m3_all_zone_name="maison" # Name of all zone
 var_jv_pg_m3_zone_1_name="cuisine" # Name of the zone 1
 var_jv_pg_m3_zone_2_name="salon" # Name of the zone 2
 var_jv_pg_m3_zone_3_name="salle de bain" # Name of the zone 3
