29bef739ee1ebf90ddde98794f9a26f0c0240764
Quentin Comte-Gaz <quentin@comte-gaz.com>
2017-04-15 17:24:32 +0200

    plugin: Fix milor issue

--- fr/functions.sh
+++ fr/functions.sh
@@ -4,7 +4,7 @@ jv_pg_m3_lang()
 {
   case "$1" in
     mac_address_is) echo "L'adresse mac du module milight est $2";;
-    invalid_mac_address) echo "Impossible de récupérer l'adresse mac...";;
+    invalid_mac_address) echo "Impossible de récupérer l'adresse mac du module milight...";;
     *) jv_error "Erreur: Clef de traduction '$1' non reconnue. Merci de contacter le concepteur de ce plugin.";;
   esac
 }
--- functions.sh
+++ functions.sh
@@ -7,6 +7,6 @@ jv_pg_m3_getMacAddress()
   if [[ $result =~ ^[0-9a-fA-F]{2}:[0-9a-fA-F]{2}:[0-9a-fA-F]{2}:[0-9a-fA-F]{2}:[0-9a-fA-F]{2}:[0-9a-fA-F]{2}$ ]]; then
     say "$(jv_pg_m3_lang mac_address_is $result)"
   else
-    say "$(jv_pg_aj_lang invalid_mac_address)"
+    say "$(jv_pg_m3_lang invalid_mac_address)"
   fi
 }
